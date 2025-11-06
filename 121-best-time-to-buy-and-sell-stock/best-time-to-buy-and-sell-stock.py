fdf05f400e60ac2ab4a8c6387b4d1ccfbe65dd37
diramugodana <144637939+diramugodana@users.noreply.github.com>
2025-11-05 21:50:01 -0600

    Time: 31 ms (85.08%) | Memory: 26.8 MB (94.63%) - LeetSync

--- 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
+++ 121-best-time-to-buy-and-sell-stock/best-time-to-buy-and-sell-stock.py
@@ -1,18 +1,13 @@
 class Solution:
     def maxProfit(self, prices: List[int]) -> int:
-        # prices[i] is price of a given stock on the ith day
-        # find min and the greatest max after it, and minus
-        # brute force approach would be 
-        # for every day you buy, check all follwing, and find diff, then return max diff? 
         min_price = float('inf')
         max_profit = 0
 
         for price in prices:
             if price < min_price:
                 min_price = price
-            elif price - min_price > max_profit:
-                max_profit = price - min_price
-        return max_profit
 
-        # time complexity = O(n)
-        # space complexity = O(1)
+            if price - min_price > max_profit:
+                max_profit = max(max_profit, price - min_price)
+        return max_profit       
+        
