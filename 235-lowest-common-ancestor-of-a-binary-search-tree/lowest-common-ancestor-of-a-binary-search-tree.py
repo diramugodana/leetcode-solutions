146b4e12c8c662e7dd095c32b66270644773bb28
diramugodana <144637939+diramugodana@users.noreply.github.com>
2025-11-12 18:42:20 -0500

    Time: 56 ms (70.77%) | Memory: 21.2 MB (19.94%) - LeetSync

--- 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
+++ 235-lowest-common-ancestor-of-a-binary-search-tree/lowest-common-ancestor-of-a-binary-search-tree.py
@@ -5,22 +5,44 @@
 #         self.left = None
 #         self.right = None
 
-class Solution:
-    def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
-        # begin at root
-        # set current equal to root
-        # begin traversing
-        # if both p and q values are less than root, curr = curr.left
-        # if both more, curr = curr.right
-        # otherwise, either they are on diff subtrees or one is the parent/ ancestor of the other
-        # therefore it will be the lca (first common point)
-        current = root
+# class Solution:
+#     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+#         if not root:
+#             return -1
+        
+#         # initialize a variable current so we can traverse the tree
+#         current = root
 
-        while current:
-            if p.val < current.val and q.val < current.val:
-                current = current.left
-            elif p.val > current.val and q.val > current.val:
-                current = current.right
-            else: # two possibilities
-                return current
-            
+#         while current:
+#             if p.val < current.val and q.val < current.val: # both node values less than node.val
+#                 current = current.left
+
+#             elif p.val > current.val and q.val > current.val: # both node values less than node.val
+#                 current = current.right
+#             # remaining case: split point is either current node, or root
+#             else:
+#                 return current
+
+# # time complexity = O(h)
+# # space complexity = O(1)
+
+# recursive
+# A function is a standalone block of code (defined at the top level).
+# A method is a function bound to an instance of a class — it automatically receives self as the first argument.
+class Solution:
+     def lowestCommonAncestor(self, root: 'TreeNode', p: 'TreeNode', q: 'TreeNode') -> 'TreeNode':
+
+        # base case
+        if not root:
+            return None # exp output: a root node
+        # recursive case
+        if p.val < root.val and q.val < root.val:
+            return self.lowestCommonAncestor(root.left, p, q)
+        if p.val > root.val and q.val > root.val:
+            return self.lowestCommonAncestor(root.right, p, q)
+        else:
+            return root
+
+
+
+        
